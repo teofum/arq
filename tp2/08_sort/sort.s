2aa6e947ade32fdd783f4beb475f205e71a27624
Teo Fumagalli <teo.fum@gmail.com>
2025-03-27 15:01:02 -0300

    tp2 08 sort

--- tp2/08_sort/sort.s
+++ tp2/08_sort/sort.s
@@ -4,17 +4,18 @@ global _start
 extern write_stdout
 extern exit
 extern num2str
-extern min
+extern sort
 
 _start:
-    ; Get minimum of array
+    ; Sort array
     mov eax, arr
     mov ebx, [len]
-    call min
+    call sort
 
+loop:
     ; Print to string and write to stdout
-    mov eax, [eax]
-    push eax
+    mov ecx, [eax]
+    push ecx
     mov ecx, ost
     push ecx
     call num2str
@@ -22,10 +23,14 @@ _start:
     mov edx, oln
     call write_stdout
 
+    add eax, 4
+    dec ebx
+    jnz loop
+
     call exit
 
 section .data
 arr dd  9, 5, 10, 3, 1, 4, 2, 7, 6, 8
 len dd 10
-ost db "     ", 10
+ost db "   , "
 oln equ $-ost
--- tp2/lib/arrays.s
+++ tp2/lib/arrays.s
@@ -5,7 +5,7 @@ global min
 ; min
 ;
 ; Returns the minimum element of an array of 4-byte signed integers
-; EAX: pointer to array start (in), minimum value (out)
+; EAX: pointer to array start (in), pointer to minimum value (out)
 ; EBX: length of array
 ;
 ; Example call
@@ -13,18 +13,19 @@ global min
 ; mov ebx len
 ; call min
 min:
+    push edx
     push ecx
     push ebx
 
     mov ecx, eax
-    mov eax, [ecx]
     
 min_loop:
     add ecx, 4
-    cmp eax, [ecx]
+    mov edx, [eax]
+    cmp edx, [ecx]
     jle min_next
 
-    mov eax, [ecx]
+    mov eax, ecx
     
 min_next:
     dec ebx
@@ -32,4 +33,44 @@ min_next:
 
     pop ebx
     pop ecx
+    pop edx
     ret
+
+global sort
+
+; sort
+;
+; Sorts an array of 4-byte signed integers in place
+; EAX: pointer to array start
+; EBX: length of array
+;
+; Example call
+; mov eax array
+; mov ebx len
+; call sort
+sort:
+    pushad
+
+sort_loop:
+    ; Find minimum element of array
+    push eax
+    call min
+    mov ecx, eax    ; ECX <- ptr to minimum
+    pop eax
+
+    ; Swap first element with min
+    push ebx
+    mov edx, [ecx]  ; EDX <- min
+    mov ebx, [eax]  ; EBX <- first
+    mov [ecx], ebx
+    mov [eax], edx
+    pop ebx
+
+    ; Move to next element, sort the rest of the array
+    add eax, 4
+    dec ebx
+    jnz sort_loop
+
+    popad
+    ret
+
