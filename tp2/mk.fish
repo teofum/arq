8509b4bfd8edf6427ffd3ad3c87ee8d659ba8a95
Teo Fumagalli <teo.fum@gmail.com>
2025-03-27 11:40:58 -0300

    better mk script

--- tp2/mk.fish
+++ tp2/mk.fish
@@ -4,6 +4,8 @@ set -l num $argv[1]
 set -l name $argv[2]
 set -l dir_name (string join _ $num $name)
 
+set -l libs $argv[3..]
+
 mkdir $dir_name
 
 echo "section .text
@@ -16,3 +18,10 @@ _start:
         int 80h
 
 section .data" >> $dir_name/$name.s
+
+echo "
+$name: $dir_name.o $libs
+	ld -m elf_i386 $dir_name.o $libs -o $name
+
+$dir_name.o: $dir_name/$name.s
+	nasm -f elf32 $dir_name/$name.s -o $dir_name.o" >> Makefile
