9c0ee7932c8b333411759912888367883ad8afbf
Teo Fumagalli <teo.fum@gmail.com>
2025-03-29 16:33:44 -0300

    tp2 10 argv

--- tp2/10_argv/argv.s
+++ tp2/10_argv/argv.s
@@ -2,9 +2,59 @@ section .text
 global _start
 
 extern exit
+extern num2str
+extern argc
+extern argv
+extern print
+extern println
 
 _start:
+    mov ebp, esp
+    call argc
+
+    ; Initialize
+    mov ebx, eax    ; EBX <- arg count
+    mov ecx, 0      ; ECX <- arg index
+    
+    ; Print arg count
+    push eax
+    mov eax, num
+    push eax
+    call num2str
+
+    mov eax, arg_str
+    call print
+    mov eax, num
+    call println
+    
+loop:
+    mov eax, begin
+    call print
+    
+    ; Print arg number
+    mov eax, ecx
+    push eax
+    mov eax, num
+    push eax
+    call num2str
+    call print
+
+    mov eax, close
+    call print
+
+    ; Print arg value
+    mov eax, ecx
+    call argv       ; EAX <- arg value ptr
+    call println
+
+    inc ecx
+    cmp ecx, ebx
+    jne loop
 
     call exit
 
 section .data
+arg_str db  "argc: ", 0
+begin   db  "argv[", 0
+close   db  "]: ", 0
+num     db  "    "
