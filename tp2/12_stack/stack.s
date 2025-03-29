653116fc03d343b3f10ff5cc15c2161a7cfa3697
Teo Fumagalli <teo.fum@gmail.com>
2025-03-29 20:47:55 -0300

    tp2 12 stack

---
+++ tp2/12_stack/stack.s
@@ -0,0 +1,48 @@
+section .text
+global _start
+
+extern exit
+extern num2str
+extern print
+extern println
+
+_start:
+    mov ebp, esp
+    mov eax, 0      ; Byte counter
+    mov ecx, 0      ; Zero counter
+
+loop:
+    mov ebx, [ebp + eax]
+    cmp ebx, 0
+    jnz continue
+
+    ; If the current byte is zero check ECX
+    inc ecx
+    cmp ecx, 2      ; First zero marks end of args, second end of envvars
+    je  end         ; Stack ends after envvars
+
+continue:
+    add eax, 4
+    jmp loop
+
+end:
+    mov edx, eax
+    mov eax, str_begin
+    call print
+    mov eax, edx
+
+    push eax
+    mov eax, string
+    push eax
+    call num2str
+    call print
+
+    mov eax, str_end
+    call println
+
+    call exit
+
+section .data
+str_begin   db  "Stack is ", 0
+str_end     db  " bytes long", 0
+string      db  "                "
