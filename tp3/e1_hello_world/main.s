f8f4313f9edac6c019e79855f730d6f02f423b99
Teo Fumagalli <teo.fum@gmail.com>
2025-04-09 17:05:49 -0300

    tp3 e1 return from main

--- tp3/e1_hello_world/main.s
+++ tp3/e1_hello_world/main.s
@@ -6,8 +6,7 @@ extern hello_world
 main:
     call hello_world
 
-    mov eax, 01h    ; exit
-    mov ebx, 0      ; exit with code 0, success
-    int 80h
+    mov eax, 0      ; exit with code 0, success
+    ret             ; we're in a c env, so we can exit by returning from main
 
 section .data
