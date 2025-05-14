4b1ef193c60e8095dfffe53141e6644cb0ba3356
Teo Fumagalli <teo.fum@gmail.com>
2025-05-14 16:52:50 -0300

    tp5 e1

---
+++ tp5/x64BareBones/Kernel/include/naiveConsole.h
@@ -0,0 +1,31 @@
+#ifndef NAIVE_CONSOLE_H
+#define NAIVE_CONSOLE_H
+
+#include <stdint.h>
+
+typedef enum {
+  C_BLACK = 0x0,
+  C_BLUE,
+  C_GREEN,
+  C_CYAN,
+  C_RED,
+  C_MAGENTA,
+  C_YELLOW,
+  C_WHITE
+} color_t;
+
+#define FG(x) (x)
+#define BG(x) ((x) << 4)
+
+void ncPrint(const char *string);
+void ncPrintChar(char character);
+void ncPrintColor(const char *str, uint8_t color);
+void ncPrintCharColor(char c, uint8_t color);
+void ncNewline();
+void ncPrintDec(uint64_t value);
+void ncPrintHex(uint64_t value);
+void ncPrintBin(uint64_t value);
+void ncPrintBase(uint64_t value, uint32_t base);
+void ncClear();
+
+#endif
